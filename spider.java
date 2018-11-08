9fbaae24213e3a669b750bed73827a0d30e40932
WEIWEIHUI <44855633+WEIWEIHUI@users.noreply.github.com>
2018-11-08 16:46:00 +0800

    Update spider.java

--- spider.java
+++ spider.java
@@ -1,24 +1,33 @@
-
-
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public static void main（）{
-	call_spider("2018-05","/home/loyu/info/a.xls");
+class StreamDrainer implements Runnable {
+    private InputStream ins;
+
+    public StreamDrainer(InputStream ins) {
+        this.ins = ins;
+    }
+
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(ins,"gb2312"));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
-
-
-public static int call_spider(String date ,String filename){
-    	//scrapy crawl build -a date=2018-05 -a filename=D:\\c.xls
-
+public class Main {
+	public static int call_spider(String date ,String filename){
 		String call = "cd /home/louyu/ && source env/bin/activate && cd spider/tutorial && scrapy crawl build -a date="+date +" -a filename="+filename;
-		//String call = "cmd.exe /c D: &&  echo 1234 > " + filename ;
 		Runtime runtime = Runtime.getRuntime();
     	
 		try {
@@ -38,5 +47,15 @@ public static int call_spider(String date ,String filename){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 		return 1;
+	}
+    
+    public static void main(String[] args) {
+    	long startTime = System.currentTimeMillis();//获取当前时间
+    	call_spider("2018-05","/home/louyu/info/a.xls");
+    	long endTime = System.currentTimeMillis();
+    	System.out.println("程序运行时间："+(endTime-startTime)+"ms");
+		
 	}
